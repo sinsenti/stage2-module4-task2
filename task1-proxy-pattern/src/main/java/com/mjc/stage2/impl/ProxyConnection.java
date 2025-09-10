8f1379b81e1f04611383b6fd4bc5b69fa4b7584e
sinsenti <sinsenti73@gmail.com>
2025-09-10 18:10:06 +0300

    Auto-push: 2025-09-10 18:10:06

--- task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
+++ task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
@@ -1,15 +1,27 @@
 package com.mjc.stage2.impl;
 
+import com.mjc.stage2.Connection;
 
-public class ProxyConnection {
-    private RealConnection realConnection;
+public class ProxyConnection implements Connection {
+  private RealConnection realConnection;
 
-    public ProxyConnection(RealConnection realConnection) {
-        this.realConnection = realConnection;
-    }
+  public ProxyConnection(RealConnection realConnection) {
+    this.realConnection = realConnection;
+  }
 
-    public void reallyClose() {
-        // Write your code here!
-    }
-    // Implement methods here!
+  public void reallyClose() {
+    realConnection.close();
+  }
+
+  @Override
+  public void close() {
+    ConnectionPool connectionPool = ConnectionPool.getInstance();
+    connectionPool.releaseConnection(this);
+  }
+
+  @Override
+  public boolean isClosed() {
+    return realConnection.isClosed();
+  }
+
 }
